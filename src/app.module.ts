a7939c2a50dc16be11ec595f9754df631a65736a
Тимерьян Насибуллин <timerannasibullin@MacBook-Air-Timeran.local>
2023-05-22 05:09:48 +0500

    Инициализировал модели

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+const dataBaseInstance = SequelizeModule.forRoot({
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'timerannasibullin',
+  password: 'root',
+  database: 'chat',
+  autoLoadModels: true,
+  models: [],
+});
 
 @Module({
-  imports: [],
+  imports: [dataBaseInstance],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/user.model.ts
@@ -0,0 +1,27 @@
+import { Column, DataType, HasMany, Model, Table } from 'sequelize-typescript';
+import Chat from '../chat/chat.model';
+
+@Table
+export class User extends Model {
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    unique: true,
+  })
+  id: number;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  username: string;
+
+  @Column({
+    defaultValue: [],
+    allowNull: false,
+  })
+  @HasMany(() => Chat)
+  chats: Chat[];
+}
